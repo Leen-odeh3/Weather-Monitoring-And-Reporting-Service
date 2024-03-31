93fef6434e17078150f69304fe82e33428980322
Leen Odeh <leenodeh287@gmail.com>
2024-03-31 15:45:48 +0300

    add ITextFormatStrategy

---
+++ Weather-Monitoring-And-Reporting-Service/Strategies/ITextFormatStrategy.cs
@@ -0,0 +1,9 @@
+﻿
+using static Weather_Monitoring_And_Reporting_Service.Weather;
+
+namespace Weather_Monitoring_And_Reporting_Service.Strategies;
+
+public interface ITextFormatStrategy
+{
+    public WeatherData GetWeatherData(string text);
+}
