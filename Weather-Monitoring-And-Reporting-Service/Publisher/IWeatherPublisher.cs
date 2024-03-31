79421f3edf404108cb3e08037e96b4d9db98fd79
Leen Odeh <leenodeh287@gmail.com>
2024-03-31 15:24:08 +0300

    add IWeatherPublisher/ attach, detach,notify.

---
+++ Weather-Monitoring-And-Reporting-Service/Publisher/IWeatherPublisher.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Weather_Monitoring_And_Reporting_Service.Subscriber;
+
+namespace Weather_Monitoring_And_Reporting_Service.Publisher;
+
+public interface IWeatherPublisher
+{
+    public void Attach(IWeatherSubscriber observer);
+
+    public void Detach(IWeatherSubscriber observer);
+
+    public void Notify();
+}
