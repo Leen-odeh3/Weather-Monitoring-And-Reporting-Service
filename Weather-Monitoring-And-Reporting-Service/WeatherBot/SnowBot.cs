2794649e45603d97a85f2ac2f06ee8d64bedb2d9
Leen Odeh <leenodeh287@gmail.com>
2024-03-31 15:37:27 +0300

    SnowBot Updated

--- Weather-Monitoring-And-Reporting-Service/WeatherBot/SnowBot.cs
+++ Weather-Monitoring-And-Reporting-Service/WeatherBot/SnowBot.cs
@@ -1,12 +1,22 @@
 ﻿
 
+using static Weather_Monitoring_And_Reporting_Service.Weather;
+
 namespace Weather_Monitoring_And_Reporting_Service.WeatherBot;
 
-public class SnowBot 
+public class SnowBot : IWeatherBotSubscriber
 {
     public bool Enabled { get; init; }
     public int TemperatureThreshold { get; init; }
     public string Message { get; init; }
 
-   
+    public void ProcessWeatherUpdate(WeatherData weatherData)
+    {
+        if (!Enabled) return;
+        if (weatherData.Temperature < TemperatureThreshold)
+        {
+            Console.WriteLine("SnowBot Activated!");
+            Console.WriteLine($"SnowBot: \"{Message}\"");
+        }
+    }
 }
