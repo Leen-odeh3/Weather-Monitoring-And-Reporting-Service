f2079f3259eb2863fc3f4beaf22653cc1916ea49
Leen Odeh <leenodeh287@gmail.com>
2024-03-31 15:42:39 +0300

    WeatherPublisher updated

---
+++ Weather-Monitoring-And-Reporting-Service/Publisher/WeatherPublisher.cs
@@ -0,0 +1,59 @@
+﻿using static Weather_Monitoring_And_Reporting_Service.Weather;
+using Weather_Monitoring_And_Reporting_Service.WeatherBot;
+using Weather_Monitoring_And_Reporting_Service.Subscriber;
+using Weather_Monitoring_And_Reporting_Service.Configuration;
+
+namespace Weather_Monitoring_And_Reporting_Service.Publisher;
+
+public class WeatherPublisher : IWeatherPublisher
+{
+
+    private Weather _weatherData;
+    public Weather WeatherData
+    {
+        set
+        {
+            _weatherData = value;
+            Notify();
+        }
+    }
+
+    private List<IWeatherBotSubscriber> _subscribers = new List<IWeatherBotSubscriber>();
+    public WeatherPublisher(string text, ITextFormatStrategy textFormat, BotConfiguration botConfig)
+    {
+        InitializeSubscribers(botConfig);
+
+        _weatherData = textFormat.GetWeatherData(text);
+
+        Notify();
+    }
+
+    private void InitializeSubscribers(BotConfiguration botConfig)
+    {
+        Attach(botConfig.RainBot);
+        Attach(botConfig.SnowBot);
+        Attach(botConfig.SunBot);
+    }
+
+    public void Notify()
+    {
+        Console.WriteLine("Notifying observers .... ");
+        Thread.Sleep(500);
+        foreach (var subscriber in _subscribers)
+        {
+            subscriber.ProcessWeatherUpdate(_weatherData);
+            Thread.Sleep(500);
+        }
+    }
+
+    public void Attach(IWeatherSubscriber observer)
+    {
+        _subscribers.Add(observer);
+    }
+
+    public void Detach(IWeatherSubscriber observer)
+    {
+        _subscribers.Remove(observer);
+
+    }
+}
--- Weather-Monitoring-And-Reporting-Service/Subscriber/IWeatherSubscriber.cs
+++ Weather-Monitoring-And-Reporting-Service/Subscriber/IWeatherSubscriber.cs
@@ -1,11 +1,9 @@
 ﻿
 
-using Weather_Monitoring_And_Reporting_Service.Publisher;
-
 namespace Weather_Monitoring_And_Reporting_Service.Subscriber;
 
 public interface IWeatherSubscriber
 {
-    public void ProcessWeatherUpdate(IWeatherPublisher weatherDataPublisher);
+    public void ProcessWeatherUpdate(Weather weatherPublisher);
 
 }
