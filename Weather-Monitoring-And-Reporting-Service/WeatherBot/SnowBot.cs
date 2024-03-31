c829e967e3f8d2f9de38095b17e956a3badef750
Leen Odeh <leenodeh287@gmail.com>
2024-03-31 15:20:16 +0300

    add SnowBot  class

---
+++ Weather-Monitoring-And-Reporting-Service/WeatherBot/SnowBot.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Weather_Monitoring_And_Reporting_Service.WeatherBot;
+
+public class SnowBot 
+{
+    public bool Enabled { get; init; }
+    public int TemperatureThreshold { get; init; }
+    public string Message { get; init; }
+
+   
+}
