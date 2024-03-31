f2eb80613b4d020b95a63bcc07432bc9b4c173b4
Leen Odeh <leenodeh287@gmail.com>
2024-03-31 15:17:33 +0300

    Add project files.

---
+++ Weather-Monitoring-And-Reporting-Service/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Weather_Monitoring_And_Reporting_Service
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
