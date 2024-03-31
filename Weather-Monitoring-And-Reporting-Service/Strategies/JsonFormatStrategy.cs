5d6247f558c7b150f37f082f4d888161763dd93d
Leen Odeh <leenodeh287@gmail.com>
2024-03-31 15:46:39 +0300

    Json Format Strategy added

---
+++ Weather-Monitoring-And-Reporting-Service/Strategies/JsonFormatStrategy.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Text.Json;
+using static Weather_Monitoring_And_Reporting_Service.Weather;
+
+namespace Weather_Monitoring_And_Reporting_Service.Strategies;
+
+public class JsonFormatStrategy : ITextFormatStrategy
+{
+    public WeatherData GetWeatherData(string text)
+    {
+        WeatherData weatherData = JsonSerializer.Deserialize<WeatherData>(text);
+        return weatherData;
+    }
+}
