946683459a2b1c4a8436b768c554c667799febf7
Leen Odeh <leenodeh287@gmail.com>
2024-03-31 15:44:58 +0300

    add Text Format Strategy Factory

--- Weather-Monitoring-And-Reporting-Service/Program.cs
+++ Weather-Monitoring-And-Reporting-Service/Program.cs
@@ -1,11 +1,57 @@
 ﻿using System.Text.Json;
 using Weather_Monitoring_And_Reporting_Service.Configuration;
+using Weather_Monitoring_And_Reporting_Service.Publisher;
+using static Weather_Monitoring_And_Reporting_Service.Weather;
 
 namespace Weather_Monitoring_And_Reporting_Service;
+
 
 internal class Program
 {
     static void Main(string[] args)
+    {
+        #region Load Bot Configurations
+        BotConfiguration? botConfig = LoadConfig();
+
+        if (botConfig is null)
+        {
+            Console.WriteLine("An error occurred");
+            Console.WriteLine("Exiting...");
+            Thread.Sleep(2000);
+            return;
+        }
+        #endregion
+
+        #region Get Format Strategy
+        Console.Write("Enter the path to the weather data file: ");
+        string? weatherDataFilePath = Console.ReadLine();
+
+        ITextFormatStrategy? textFormatStrategy = TextFormatStrategyFactory.GetTextFormatStrategy(weatherDataFilePath);
+
+        if (textFormatStrategy is null)
+        {
+            Console.WriteLine("Cannot Handle that File");
+            Console.WriteLine("Exiting...");
+            Thread.Sleep(2000);
+            return;
+        }
+        #endregion
+
+        string weatherDataText = File.ReadAllText(weatherDataFilePath);
+        WeatherPublisher weatherDataPublisher = new WeatherPublisher(weatherDataText, textFormatStrategy, botConfig);
+
+        WeatherData newData = new WeatherData
+        {
+            Humidity = 10000,
+            Location = "New Jersy",
+            Temperature = 20000
+        };
+
+        weatherDataPublisher.WeatherData = newData;
+
+    }
+
+    private static BotConfiguration? LoadConfig()
     {
         string filePath = "config.json";
         JsonSerializerOptions options = new JsonSerializerOptions
@@ -14,19 +60,8 @@ internal class Program
             PropertyNameCaseInsensitive = true
         };
 
-
-        BotConfiguration? BotConfiguration = ManagerConfiguration.LoadConfig(filePath, options);
-
-        if (BotConfiguration is null)
-        {
-            Console.WriteLine("An error occurred");
-            Console.WriteLine("Exiting...");
-            Thread.Sleep(2000);
-            return;
-        }
-
-        Console.WriteLine(JsonSerializer.Serialize(BotConfiguration, options));
-
+        BotConfiguration? botConfig = ManagerConfiguration.LoadConfig(filePath, options);
+        return botConfig;
     }
 }
 
---
+++ Weather-Monitoring-And-Reporting-Service/TextFormatStrategyFactory.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Weather_Monitoring_And_Reporting_Service;
+
+public static class TextFormatStrategyFactory
+{
+
+    public static ITextFormatStrategy? GetTextFormatStrategy(string? weatherDataFilePath)
+    {
+        if (String.IsNullOrEmpty(weatherDataFilePath))
+        {
+            throw new ArgumentNullException(nameof(weatherDataFilePath));
+        }
+
+        string fileExtension = GetFileExtension(weatherDataFilePath);
+
+        if (fileExtension.Equals("json")) return new JsonFormatStrategy();
+        if (fileExtension.Equals("xml")) return new XmlFormatStrategy();
+
+        else return null;
+    }
+
+    private static string GetFileExtension(string? weatherDataFilePath)
+    {
+        if (String.IsNullOrEmpty(weatherDataFilePath))
+        {
+            throw new ArgumentNullException(nameof(weatherDataFilePath));
+        }
+        return weatherDataFilePath.Split('.').Last();
+    }
+}
