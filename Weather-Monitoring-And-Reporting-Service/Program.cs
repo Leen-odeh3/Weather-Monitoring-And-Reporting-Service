fed6ef21a717a5603ed10f328dadac5b20fdae65
Leen Odeh <leenodeh287@gmail.com>
2024-03-31 15:32:49 +0300

    add Weather data

--- Weather-Monitoring-And-Reporting-Service/Program.cs
+++ Weather-Monitoring-And-Reporting-Service/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Weather_Monitoring_And_Reporting_Service
+﻿using System.Text.Json;
+using Weather_Monitoring_And_Reporting_Service.Configuration;
+
+namespace Weather_Monitoring_And_Reporting_Service;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string filePath = "config.json";
+        JsonSerializerOptions options = new JsonSerializerOptions
         {
-            Console.WriteLine("Hello, World!");
+            WriteIndented = true,
+            PropertyNameCaseInsensitive = true
+        };
+
+
+        BotConfiguration? BotConfiguration = ManagerConfiguration.LoadConfig(filePath, options);
+
+        if (BotConfiguration is null)
+        {
+            Console.WriteLine("An error occurred");
+            Console.WriteLine("Exiting...");
+            Thread.Sleep(2000);
+            return;
         }
+
+        Console.WriteLine(JsonSerializer.Serialize(BotConfiguration, options));
+
     }
 }
+
---
+++ Weather-Monitoring-And-Reporting-Service/Weather.cs
@@ -0,0 +1,31 @@
+﻿
+using Weather_Monitoring_And_Reporting_Service.Publisher;
+using Weather_Monitoring_And_Reporting_Service.Subscriber;
+
+namespace Weather_Monitoring_And_Reporting_Service;
+
+internal class Weather : IWeatherPublisher
+{
+    public int Humidity { get; init; }
+    public int Temperature { get; init; }
+
+    private List<IWeatherSubscriber> _subscribers = new List<IWeatherSubscriber>();
+
+    public void Notify()
+    {
+        foreach (var subscriber in _subscribers)
+        {
+            subscriber.ProcessWeatherUpdate(this);
+        }
+    }
+
+    public void Attach(IWeatherSubscriber observer)
+    {
+        _subscribers.Add(observer);
+    }
+
+    public void Detach(IWeatherSubscriber observer)
+    {
+        _subscribers.Remove(observer);
+    }
+}
