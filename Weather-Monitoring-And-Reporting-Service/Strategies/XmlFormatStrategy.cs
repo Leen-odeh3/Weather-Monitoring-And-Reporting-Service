8d79cb1e5e36867ced387a31411056956cb7d3c0
Leen Odeh <leenodeh287@gmail.com>
2024-03-31 15:47:17 +0300

    add XML format

---
+++ Weather-Monitoring-And-Reporting-Service/Strategies/XmlFormatStrategy.cs
@@ -0,0 +1,21 @@
+﻿
+using static Weather_Monitoring_And_Reporting_Service.Weather;
+using System.Xml.Serialization;
+
+namespace Weather_Monitoring_And_Reporting_Service.Strategies;
+
+public class XmlFormatStrategy : ITextFormatStrategy
+{
+    public WeatherData GetWeatherData(string text)
+    {
+
+        XmlSerializer serializer = new XmlSerializer(typeof(WeatherData));
+
+        using (TextReader reader = new StringReader(text))
+        {
+            return (WeatherData)serializer.Deserialize(reader);
+        }
+
+    }
+
+}
