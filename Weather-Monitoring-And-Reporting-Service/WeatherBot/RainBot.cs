75bcc229b473eda9a48ea9442fd43e071b588a7f
Leen Odeh <leenodeh287@gmail.com>
2024-03-31 15:19:32 +0300

    add RainBot class

---
+++ Weather-Monitoring-And-Reporting-Service/WeatherBot/RainBot.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Weather_Monitoring_And_Reporting_Service.WeatherBot;
+
+public class RainBot { 
+    public bool Enabled { get; init; }
+    public int HumidityThreshold { get; init; }
+    public string Message { get; init; }
+
+   
+}
