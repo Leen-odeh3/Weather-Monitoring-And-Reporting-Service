29f4a9d97635af931d777cfc7b6827c52403109f
Leen Odeh <leenodeh287@gmail.com>
2024-03-31 15:25:01 +0300

    Update Subscriber Interface

--- Weather-Monitoring-And-Reporting-Service/Subscriber/IWeatherSubscriber.cs
+++ Weather-Monitoring-And-Reporting-Service/Subscriber/IWeatherSubscriber.cs
@@ -1,9 +1,11 @@
 ﻿
 
+using Weather_Monitoring_And_Reporting_Service.Publisher;
+
 namespace Weather_Monitoring_And_Reporting_Service.Subscriber;
 
 public interface IWeatherSubscriber
 {
-    public void ProcessWeatherUpdate();
+    public void ProcessWeatherUpdate(IWeatherPublisher weatherDataPublisher);
 
 }
