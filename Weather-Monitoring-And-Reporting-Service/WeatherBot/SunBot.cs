270b3eb9d12002ebc6f592714624024b8cd26f9f
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-04-17 13:51:00 +0300

    fix issues founded

--- Weather-Monitoring-And-Reporting-Service/WeatherBot/SunBot.cs
+++ Weather-Monitoring-And-Reporting-Service/WeatherBot/SunBot.cs
@@ -14,13 +14,23 @@ public class SunBot : IWeatherSubscriber
 
     public void ProcessWeatherUpdate(Weather weatherData)
     {
-        if (!Enabled) return;
+        if (!Enabled)
+        {
+            Console.WriteLine("SunBot is not enabled.");
+            return;
+        }
+
         if (weatherData.Temperature > TemperatureThreshold)
         {
             Activated = true;
             Console.WriteLine("SunBot Activated!");
             Console.WriteLine($"SunBot: \"{Message}\"");
         }
+        else
+        {
+            Console.WriteLine($"SunBot not activated. Temperature {weatherData.Temperature} is not higher than the threshold {TemperatureThreshold}.");
+        }
     }
+
 
 }
