45f7800f7b9b5c10dcdcbf4780766036997f0d7d
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-04-17 10:45:49 +0300

    add RainBot Test Model

---
+++ Weather-Monitoring-And-Reporting-Service_Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Weather_Monitoring_And_Reporting_Service_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Weather-Monitoring-And-Reporting-Service_Tests/WeatherBotTests/RainBotTest.cs
@@ -0,0 +1,52 @@
+﻿
+
+using Weather_Monitoring_And_Reporting_Service;
+using Weather_Monitoring_And_Reporting_Service.WeatherBot;
+
+namespace Weather_Monitoring_And_Reporting_Service_Tests.WeatherBotTests;
+
+public class RainBotTest
+{
+
+    [Fact]
+    public void ProcessWeatherUpdate_Disabled_MustNotBeActivated()
+    {
+        var weather= new Weather { Humidity = 60 };
+        var sut = new RainBot
+        {
+            Enabled = false,
+            HumidityThreshold = 70,
+            Message = "It's raining!"
+        };
+        sut.ProcessWeatherUpdate(weather);
+        Assert.False(sut.Activated);
+    }
+    [Fact]
+
+    public void ProcessWeatherUpdate_EnabledWithHumidityLowerThanHumidityThreshold_MustNotBeActivated()
+    {
+        var weatherData = new Weather { Humidity = 60 };
+        var sut = new RainBot
+        {
+            Enabled = true,
+            HumidityThreshold = 100,
+            Message = "It's raining!"
+        };
+        sut.ProcessWeatherUpdate(weatherData);
+        Assert.False(sut.Activated);
+    }
+    [Fact]
+
+    public void ProcessWeatherUpdate_EnabledWithHumidityHigherThanHumidityThreshold_MustBeActivated()
+    {
+        var weatherData = new Weather { Humidity = 60 };
+        var sut = new RainBot
+        {
+            Enabled = true,
+            HumidityThreshold = 40,
+            Message = "It's raining!"
+        };
+        sut.ProcessWeatherUpdate(weatherData);
+        Assert.True(sut.Activated);
+    }
+}
