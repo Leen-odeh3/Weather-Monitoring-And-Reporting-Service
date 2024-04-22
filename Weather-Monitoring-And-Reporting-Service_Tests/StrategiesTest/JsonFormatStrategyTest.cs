d65714d446d446bcb3a67eee33e98b09adc0bbc2
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-04-22 14:42:22 +0300

    add more Tests.

---
+++ Weather-Monitoring-And-Reporting-Service_Tests/StrategiesTest/JsonFormatStrategyTest.cs
@@ -0,0 +1,39 @@
+﻿
+using Weather_Monitoring_And_Reporting_Service.Strategies;
+using Weather_Monitoring_And_Reporting_Service;
+using System.Text.Json;
+
+namespace Weather_Monitoring_And_Reporting_Service_Tests.StrategiesTest;
+
+ public class JsonFormatStrategyTest
+{
+
+    [Fact]
+    public void GetWeatherData_ValidJson_ReturnsWeatherObject()
+    {
+        // Arrange
+        string json = "{\"Temperature\":25,\"Humidity\":50}";
+        JsonFormatStrategy strategy = new JsonFormatStrategy();
+
+        // Act
+        Weather weather = strategy.GetWeatherData(json);
+
+        // Assert
+        Assert.NotNull(weather);
+        Assert.Equal(25, weather.Temperature);
+        Assert.Equal(50, weather.Humidity);
+    }
+
+    [Fact]
+    public void GetWeatherData_InvalidJson_ThrowsException()
+    {
+        // Arrange
+        string json = "Invalid JSON";
+        JsonFormatStrategy strategy = new JsonFormatStrategy();
+
+        // Act & Assert
+        Assert.Throws<JsonException>(() => strategy.GetWeatherData(json));
+    }
+}
+
+
